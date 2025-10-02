18dab2a355f1c974a27c22cebb649c0d9aa71682
Luiz Henrique <alvesluizhenrique3@gmail.com>
2025-10-02 12:03:14 -0300

    criando teste do model user

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from .models import models
 
-# Create your tests here.
+User = get_user_model()
+
+class CustomUserManagerTests(TestCase):
+    def test_create_user_with__user_password(self):
+        user = User.objects.create_user(
+            email = "user@example.com",
+            password = "password123"
+        )
+        self.assertEqual(user.email, "user@example.com")
+        self.assertTrue(user.check_password("password123"))
+        self.assertFalse(user.is_superuser)
+
+
+    
