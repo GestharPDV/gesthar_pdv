ee2c3fe1e154c33d6bbaa06aaa230bd6d3d09f61
cwesly <cauawesly@outlook.com>
2026-03-23 22:25:06 -0300

    fix: Mudança de cadastro de usuário para Accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import login_view, logout_view, password_change_view, register_view
+from .views import login_view, logout_view, password_change_view, register_view, user_create_view
 
 app_name = "accounts"
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path("logout/", logout_view, name="logout"),
     path("register/", register_view, name="register"),
     path("password_change/", password_change_view, name="user-password-change"),
+    path("user/create/", user_create_view, name="user-create"),
 ]
